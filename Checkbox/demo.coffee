c3c76e8f07f1c6fe963958281345264efee28d42
Devin Yin <devinyin@augmentum.com.cn>
2017-02-13 18:52:13 +0800

    add the check-box with checkbox

--- Checkbox/demo.coffee
+++ Checkbox/demo.coffee
@@ -16,13 +16,36 @@
             scope.onChange()() if angular.isFunction scope.onChange()
     }
 
+  CheckBox2 = ($timeout) ->
+    return {
+      restrict: 'E'
+      scope:
+        ngModel: '='
+        onChange: '&'
+      replace: true
+      transclude: true
+      template: '<label class="checkbox" ng-class="{\'check\': ngModel}">
+                  <input type="checkbox" ng-model="ngModel" ng-click="check()"/>
+                  <span ng-transclude></span>
+                </label>'
+      link: (scope, element, attrs) ->
+        scope.check = ->
+          $timeout ->
+            scope.onChange()() if angular.isFunction scope.onChange()
+    }
+
   CheckCtrl = ($scope) ->
     $scope.check = true
+    $scope.check2 = false
 
     $scope.checkCallBcak = ->
       console.log $scope.check
 
+    $scope.checkCallBcak2 = ->
+      console.log $scope.check2
+
   module.directive 'checkBox', ['$timeout', CheckBox]
+  module.directive 'checkBoxWithCheckbox', ['$timeout', CheckBox2]
   module.controller 'CheckCtrl', CheckCtrl
   CheckCtrl.$inject = ['$scope']
 )()
--- Checkbox/demo.js
+++ Checkbox/demo.js
@@ -1,5 +1,5 @@
 (function() {
-  var CheckBox, CheckCtrl, module;
+  var CheckBox, CheckBox2, CheckCtrl, module;
   module = angular.module('app', []);
   CheckBox = function($timeout) {
     return {
@@ -23,13 +23,39 @@
       }
     };
   };
+  CheckBox2 = function($timeout) {
+    return {
+      restrict: 'E',
+      scope: {
+        ngModel: '=',
+        onChange: '&'
+      },
+      replace: true,
+      transclude: true,
+      template: '<label class="checkbox" ng-class="{\'check\': ngModel}"> <input type="checkbox" ng-model="ngModel" ng-click="check()"/> <span ng-transclude></span> </label>',
+      link: function(scope, element, attrs) {
+        return scope.check = function() {
+          return $timeout(function() {
+            if (angular.isFunction(scope.onChange())) {
+              return scope.onChange()();
+            }
+          });
+        };
+      }
+    };
+  };
   CheckCtrl = function($scope) {
     $scope.check = true;
-    return $scope.checkCallBcak = function() {
+    $scope.check2 = false;
+    $scope.checkCallBcak = function() {
       return console.log($scope.check);
+    };
+    return $scope.checkCallBcak2 = function() {
+      return console.log($scope.check2);
     };
   };
   module.directive('checkBox', ['$timeout', CheckBox]);
+  module.directive('checkBoxWithCheckbox', ['$timeout', CheckBox2]);
   module.controller('CheckCtrl', CheckCtrl);
   return CheckCtrl.$inject = ['$scope'];
 })();
